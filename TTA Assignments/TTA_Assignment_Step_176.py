8af2803378bb9f70c90381fd9b6115917c6ce72d
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-08-02 14:28:41 -0700

    Create TTA Assignment Step 176

---
+++ TTA Assignments/TTA_Assignment_Step_176.py
@@ -0,0 +1,27 @@
+
+class Item:
+    skewNumber = int()
+    quantity = int()
+    departmentID = int()
+    price = int()
+
+class Fruit(Item):
+    fruitType = ''
+    expirationDate = ''
+
+class Alchohol(Item):
+    alchoholType = ''
+    refrigerationNeeded = bool()
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    
+
+    
