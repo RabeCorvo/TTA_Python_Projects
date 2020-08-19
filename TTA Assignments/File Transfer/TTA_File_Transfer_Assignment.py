5c50c9ccc77c3a498301d2adc64fc796278a2ce9
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-08-19 16:12:05 -0700

    Update File Transfer

--- TTA Assignments/File Transfer/TTA_File_Transfer_Assignment.py
+++ TTA Assignments/File Transfer/TTA_File_Transfer_Assignment.py
@@ -1,5 +1,7 @@
 
-
+from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
 import shutil
 import os
 import time
@@ -8,13 +10,13 @@ import TTA_File_Transfer_Assignment_GUI as ftagui
 
 
 def sortFile(self):
-    fPathList = os.listdir(self.source)                                      #Lists all files in source directory ex: 'filename.txt')
+    fPathList = os.listdir(self.source)                                 #Lists all files in source directory ex: 'filename.txt')
     fullPathAndModTime = {}                                             #Creates an empty set/dictionary
     timeNow = datetime.datetime.now()                                   #Creates variable of current time ex: 2020-08-19 11:52:15.096446
     last24Hours = timeNow - datetime.timedelta(hours = 24)              #Creates variable of time 24 hours ago time ex: 2020-08-18 11:52:15.096446
     filesToSend = []                                                    #Creates an empty List
     for i in fPathList:
-        fullPath = os.path.join(self.source,i)                               #Gets full path with file name in folder: 'directory/file.ext'
+        fullPath = os.path.join(self.source,i)                          #Gets full path with file name in folder: 'directory/file.ext'
         modificationTime = os.path.getmtime(fullPath)                   #Gets modification time of file since last epoch: '1596312495.2747788'
         modTime = time.ctime(modificationTime)                          #Gets modification time in a better format: 'WkD Mon D# Hr:Min:Sec Year'
         structuredModTime = datetime.datetime.strptime(modTime, "%c")   #Changes the MOD time to a datetime object: 'Year-Month-Day Hr:Min:Sec'
@@ -27,6 +29,17 @@ def sortFile(self):
     for i in filesToSend:
             shutil.copy(self.source+i, self.destination)
 
+        
+def cancel(self):
+    self.master.destroy()
+
+def Browse1(self):
+    self.source = tk.filedialog.askdirectory()
+    self.txtBrowse1.insert(0, self.source)
+
+def Browse2(self):
+    self.destination = tk.filedialog.askdirectory()
+    self.txtBrowse2.insert(0, self.destination)
 
 if __name__ == "__main__":
-    sortFile()
+    pass
--- TTA Assignments/File Transfer/TTA_File_Transfer_Assignment_GUI.py
+++ TTA Assignments/File Transfer/TTA_File_Transfer_Assignment_GUI.py
@@ -1,12 +1,12 @@
 
+
 from tkinter import *
-from tkinter import filedialog
 import tkinter as tk
 import TTA_File_Transfer_Assignment as fta
 
 class ParentWindow(Frame):
     def __init__ (self, master):
-        Frame.__init__ (self)
+        Frame.__init__ (self, master)
 
         self.master = master
         self.master.resizable(width=False, height=False)
@@ -14,36 +14,26 @@ class ParentWindow(Frame):
         self.master.title('Check files')
 
 
-        self.btnBrowse1 = tk.Button(self.master, text='Source File', width=12, height=1, command=self.Browse1)
+        self.btnBrowse1 = tk.Button(self.master, text='Source File', width=12, height=1, command=fta.Browse1(self))
         self.btnBrowse1.grid(row=1, column=0, padx=(30,0), pady=(60,0), sticky=SE)
-        self.btnBrowse2 = tk.Button(self.master, text='Destination File', width=12, height=1, command=self.Browse2)
+        self.btnBrowse2 = tk.Button(self.master, text='Destination File', width=12, height=1, command=fta.Browse2(self))
         self.btnBrowse2.grid(row=2, column=0, padx=(30,0), pady=(10,0), sticky=SE)
-        self.btnCheckFile = tk.Button(self.master, text='File Check', width=12, height=2, command=fta.sortFile)
+        self.btnCheckFile = tk.Button(self.master, text='File Check', width=12, height=2, command=fta.sortFile(self))
         self.btnCheckFile.grid(row=3, column=0, padx=(30,0), pady=(10,0), sticky=SE)
-        self.btnCloseProgram = tk.Button(self.master, text='Close Program', width=12, height=2, command= self.cancel)
+        self.btnCloseProgram = tk.Button(self.master, text='Close Program', width=12, height=2, command=fta.cancel(self))
         self.btnCloseProgram.grid(row=3, column=3, padx=(30,30), pady=(10,0), sticky=SE)
         
         
-        self.txtBrowse1 = Entry(self.master, font=("Helvetica", 10), fg='black')
+        self.txtBrowse1 = tk.Entry(self.master, font=("Helvetica", 10), fg='black')
         self.txtBrowse1.grid(row=1, column=1, columnspan=4, padx=(30,30), pady=(60,0), sticky=W+E+N+S)
-        self.txtBrowse2 = Entry(self.master, font=("Helvetica", 10), fg='black')
+        self.txtBrowse2 = tk.Entry(self.master, font=("Helvetica", 10), fg='black')
         self.txtBrowse2.grid(row=2, column=1, columnspan=4, padx=(30,30), pady=(10,0), sticky=W+E+N+S)
         root.grid_columnconfigure(3, weight=1)
-        
-    def cancel(self):
-        self.master.destroy()
 
-    def Browse1(self):
-        self.source = tk.filedialog.askdirectory()
-        self.txtBrowse1.insert(0, self.source)
 
-    def Browse2(self):
-        self.destination = tk.filedialog.askdirectory()
-        self.txtBrowse2.insert(0, self.destination)
-        
         
 if __name__ == "__main__":
-    root = Tk()
+    root = tk.Tk()
     App = ParentWindow(root)
     root.mainloop()
                     
