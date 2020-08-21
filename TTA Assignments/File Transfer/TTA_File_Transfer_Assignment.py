72503a0c0e26e62a8353a46efdc36092df416291
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-08-21 12:03:48 -0700

    Updated File Transfer Assignment

--- TTA Assignments/File Transfer/TTA_File_Transfer_Assignment.py
+++ TTA Assignments/File Transfer/TTA_File_Transfer_Assignment.py
@@ -7,39 +7,36 @@ import os
 import time
 import datetime
 import TTA_File_Transfer_Assignment_GUI as ftagui
+        
+def cancel(self):
+        self.master.destroy()
 
+def Browse1(self):
+        source = tk.filedialog.askdirectory()
+        slash = '/'
+        self.txtBrowse1.insert(0,source)
+        self.sourceDir = source + '/'
+
+def Browse2(self):
+        destination = tk.filedialog.askdirectory()
+        self.txtBrowse2.insert(0,destination)
+        self.destinationDir = destination + '/'
 
 def sortFile(self):
-    fPathList = os.listdir(self.source)                                 #Lists all files in source directory ex: 'filename.txt')
-    fullPathAndModTime = {}                                             #Creates an empty set/dictionary
+    source = self.sourceDir
+    destination = self.destinationDir
+    fPathList = os.listdir(source)                                      #Lists all files in source directory ex: 'filename.txt')
     timeNow = datetime.datetime.now()                                   #Creates variable of current time ex: 2020-08-19 11:52:15.096446
     last24Hours = timeNow - datetime.timedelta(hours = 24)              #Creates variable of time 24 hours ago time ex: 2020-08-18 11:52:15.096446
     filesToSend = []                                                    #Creates an empty List
     for i in fPathList:
-        fullPath = os.path.join(self.source,i)                          #Gets full path with file name in folder: 'directory/file.ext'
+        fullPath = os.path.join(source,i)                               #Gets full path with file name in folder: 'directory/file.ext'
         modificationTime = os.path.getmtime(fullPath)                   #Gets modification time of file since last epoch: '1596312495.2747788'
         modTime = time.ctime(modificationTime)                          #Gets modification time in a better format: 'WkD Mon D# Hr:Min:Sec Year'
         structuredModTime = datetime.datetime.strptime(modTime, "%c")   #Changes the MOD time to a datetime object: 'Year-Month-Day Hr:Min:Sec'
-        fullPathAndModTime[i] = structuredModTime                       #Adds the full path and datetime mod time as key value pairs to a dictionary: 'directory/file.ext' : 'Year-Month-Day Hr:Min:Sec'
-    for i in fullPathAndModTime.values():                               #Compares the datetime mod time to current time
-        if i >= last24Hours and i <= timeNow:
-            for key, val in fullPathAndModTime.items():
-                if val == i:
-                    filesToSend.append(key)
-    for i in filesToSend:
-            shutil.copy(self.source+i, self.destination)
+        if structuredModTime >= last24Hours and structuredModTime <= timeNow:
+                shutil.copy(source+i, destination)
 
         
-def cancel(self):
-    self.master.destroy()
-
-def Browse1(self):
-    self.source = tk.filedialog.askdirectory()
-    self.txtBrowse1.insert(0, self.source)
-
-def Browse2(self):
-    self.destination = tk.filedialog.askdirectory()
-    self.txtBrowse2.insert(0, self.destination)
-
 if __name__ == "__main__":
     pass
--- TTA Assignments/File Transfer/TTA_File_Transfer_Assignment_GUI.py
+++ TTA Assignments/File Transfer/TTA_File_Transfer_Assignment_GUI.py
@@ -13,25 +13,22 @@ class ParentWindow(Frame):
         self.master.geometry('{}x{}'.format(800, 190))
         self.master.title('Check files')
 
-
-        self.btnBrowse1 = tk.Button(self.master, text='Source File', width=12, height=1, command=fta.Browse1(self))
+        self.btnBrowse1 = tk.Button(self.master, text='Source File', width=12, height=1, command=lambda: fta.Browse1(self))
         self.btnBrowse1.grid(row=1, column=0, padx=(30,0), pady=(60,0), sticky=SE)
-        self.btnBrowse2 = tk.Button(self.master, text='Destination File', width=12, height=1, command=fta.Browse2(self))
+        self.btnBrowse2 = tk.Button(self.master, text='Destination File', width=12, height=1, command=lambda: fta.Browse2(self))
         self.btnBrowse2.grid(row=2, column=0, padx=(30,0), pady=(10,0), sticky=SE)
-        self.btnCheckFile = tk.Button(self.master, text='File Check', width=12, height=2, command=fta.sortFile(self))
+        self.btnCheckFile = tk.Button(self.master, text='File Check', width=12, height=2, command=lambda: fta.sortFile(self))
         self.btnCheckFile.grid(row=3, column=0, padx=(30,0), pady=(10,0), sticky=SE)
-        self.btnCloseProgram = tk.Button(self.master, text='Close Program', width=12, height=2, command=fta.cancel(self))
+        self.btnCloseProgram = tk.Button(self.master, text='Close Program', width=12, height=2, command=lambda: fta.cancel(self))
         self.btnCloseProgram.grid(row=3, column=3, padx=(30,30), pady=(10,0), sticky=SE)
-        
-        
+               
         self.txtBrowse1 = tk.Entry(self.master, font=("Helvetica", 10), fg='black')
         self.txtBrowse1.grid(row=1, column=1, columnspan=4, padx=(30,30), pady=(60,0), sticky=W+E+N+S)
         self.txtBrowse2 = tk.Entry(self.master, font=("Helvetica", 10), fg='black')
         self.txtBrowse2.grid(row=2, column=1, columnspan=4, padx=(30,30), pady=(10,0), sticky=W+E+N+S)
         root.grid_columnconfigure(3, weight=1)
 
-
-        
+       
 if __name__ == "__main__":
     root = tk.Tk()
     App = ParentWindow(root)
